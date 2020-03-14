36490ad5e564dfe4cf22513d5432e051904cc3d9
mila-mamat <59339564+mila-mamat@users.noreply.github.com>
2020-03-14 17:05:50 -0400

    Added save all btn and trash btn
    added  trash btn to each row, and a save all btn at the top

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
 //render stored events to html
-$.each(localStorage, function render(key, value) {
+$.each(localStorage, function(key, value) {
   $("#" + key).find("textarea").val(value);
 });
 
 //clear out local storage and events rendered earlier
-$("#clear").click(function() {
+$("#clearAll").click(function() {
   localStorage.clear();
   $("textarea").each(function() {
     $(this).val("");
   });
 });
 
+//save events entered all together
+$("#saveAll").click(function() {
+  let events = $("textarea").toArray();
+  $.each(events, function(index,textArea) {
+    if(textArea.value!==""){
+      localStorage.setItem(index+9, textArea.value)
+    }
+  });
+});
+
+
 //save user's event into local storage using the hours as key
-$(".save").click(function() {
-  let eventContent = $(this).prev().find("textarea").val();
-  let eventTime = $(this).parent().attr("id");
+$(".saveBtn").click(function() {
+  let eventContent = $(this).parent().parent().siblings().find("textarea").val();
+  let eventTime = $(this).parent().parent().parent().attr("id");
   localStorage.setItem(eventTime, eventContent);
+});
+
+//delete user's event into local storage using the hours as key
+$(".trashBtn").click(function() {
+  $(this).parent().parent().siblings().find("textarea").val("");
+  let eventTime = $(this).parent().parent().parent().attr("id");
+  localStorage.removeItem(eventTime);
 });
 
 //color code the events based on current time
